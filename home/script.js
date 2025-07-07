ec8e0c65426144b689c2cc80608e5c5b2785aac8
Thibaut Tbc Bujiriri <166028783+thibaut-tbc-bujiriri@users.noreply.github.com>
2025-07-07 13:32:29 +0200

    update

--- home/script.js
+++ home/script.js
@@ -1,6 +1,8 @@
-alert("BIENVENU DANS SUR LA BOUTIQUE TBC GROUPE !");
+alert("BIENVENU SUR LA BOUTIQUE TBC GROUPE !");
 
 document.getElementById("dom").onclick=
     function imprimer() {
         window.print();
     }
+
+    
